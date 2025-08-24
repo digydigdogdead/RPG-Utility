a28160cc53a182f7b26389d93d16ab0ebfa0a404
digydigdogdead <carlyle.laurent@gmail.com>
2025-08-24 12:49:34 +0100

    Complete scaffolding for SessionLog control, SessionLogs page, and NewLogWindow window

--- WpfApp1/App.xaml.cs
+++ WpfApp1/App.xaml.cs
@@ -15,9 +15,11 @@ namespace WpfApp1
         public List<Clock> Clocks = new List<Clock>();
         public List<StatTrack> Stats = new List<StatTrack>();
         public List<Memo> Memos = new List<Memo>();
+        public List<SessionLog> SessionLogs = new List<SessionLog>();
 
         public Memos? MemosPage { get; set; } = null;
         public StatTracker? StatTrackerPage { get; set; } = null;
+        public SessionLogs? SessionLogsPage { get; set; } = null;
 
 
 
---
+++ WpfApp1/Pages/SessionLogs.xaml.cs
@@ -0,0 +1,44 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows;
+using System.Windows.Controls;
+using System.Windows.Data;
+using System.Windows.Documents;
+using System.Windows.Input;
+using System.Windows.Media;
+using System.Windows.Media.Imaging;
+using System.Windows.Navigation;
+using System.Windows.Shapes;
+
+namespace WpfApp1.Pages
+{
+    /// <summary>
+    /// Interaction logic for SessionLogs.xaml
+    /// </summary>
+    public partial class SessionLogs : Page
+    {
+        public SessionLogs()
+        {
+            InitializeComponent();
+            ((Application.Current as App)!.SessionLogsPage) = this;
+            RefreshLogs();
+        }
+
+        private void addSessionLogButton_Click(object sender, RoutedEventArgs e)
+        {
+
+        }
+
+        public void RefreshLogs()
+        {
+            sessionLogsWrapPanel.Children.Clear();
+            foreach (var log in ((App)Application.Current).SessionLogs)
+            {
+                sessionLogsWrapPanel.Children.Add(log);
+            }
+        }
+    }
+}

83cdcabecdacf34ba59a13241c844f495a64e15d
digydigdogdead <carlyle.laurent@gmail.com>
2025-08-25 11:22:05 +0100

    Create scaffolding for SaveLoad page

---
+++ WpfApp1/Pages/SaveLoad.xaml.cs
@@ -0,0 +1,43 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows;
+using System.Windows.Controls;
+using System.Windows.Data;
+using System.Windows.Documents;
+using System.Windows.Input;
+using System.Windows.Media;
+using System.Windows.Media.Imaging;
+using System.Windows.Navigation;
+using System.Windows.Shapes;
+
+namespace RPGUtility.Pages
+{
+    /// <summary>
+    /// Interaction logic for SaveLoad.xaml
+    /// </summary>
+    public partial class SaveLoad : Page
+    {
+        public SaveLoad()
+        {
+            InitializeComponent();
+        }
+
+        private void saveAsButton_Click(object sender, RoutedEventArgs e)
+        {
+
+        }
+
+        private void saveButton_Click(object sender, RoutedEventArgs e)
+        {
+
+        }
+
+        private void loadButton_Click(object sender, RoutedEventArgs e)
+        {
+
+        }
+    }
+}

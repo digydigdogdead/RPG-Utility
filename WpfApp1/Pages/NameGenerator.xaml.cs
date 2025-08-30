010d250ed1f7161c5f5a17a01bbb7e196301d720
digydigdogdead <carlyle.laurent@gmail.com>
2025-08-30 13:03:23 +0100

    Complete scaffolding for new name generator page

---
+++ WpfApp1/Pages/NameGenerator.xaml.cs
@@ -0,0 +1,38 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows;
+using System.Windows.Controls;
+using System.Windows.Data;
+using System.Windows.Documents;
+using System.Windows.Input;
+using System.Windows.Media;
+using System.Windows.Media.Imaging;
+using System.Windows.Navigation;
+using System.Windows.Shapes;
+
+namespace RPGUtility.Pages
+{
+    /// <summary>
+    /// Interaction logic for NameGenerator.xaml
+    /// </summary>
+    public partial class NameGenerator : Page
+    {
+        public NameGenerator()
+        {
+            InitializeComponent();
+        }
+
+        private void generateNameButton_Click(object sender, RoutedEventArgs e)
+        {
+
+        }
+
+        private void addMemoButton_Click(object sender, RoutedEventArgs e)
+        {
+
+        }
+    }
+}

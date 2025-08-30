461b2b11881ea267a1cb511e66e0b1e8afe8578e
digydigdogdead <carlyle.laurent@gmail.com>
2025-08-30 13:22:05 +0100

    Have Name generator type box disable gender box if it isn't applicable

--- WpfApp1/Pages/NameGenerator.xaml.cs
+++ WpfApp1/Pages/NameGenerator.xaml.cs
@@ -20,6 +20,36 @@ namespace RPGUtility.Pages
     /// </summary>
     public partial class NameGenerator : Page
     {
+        List<string> maleFirstFantasyNames = new List<string>()
+        {
+            "Arin", "Borin", "Cedric", "Dain", "Eldric", "Farin", "Gorin", "Haldor", "Ivor", "Jareth", "Korin", "Loric", "Marek", "Nolan", "Orin", "Perrin", "Quinlan", "Roderic", "Soren", "Theron",
+            "Ulric", "Varek", "Wulfric", "Xander", "Yorick", "Zarek",
+            "Eden", "Finn", "Galen", "Hale", "Jace", "Kade", "Liam", "Milo", "Nash", "Oren", "Reed", "Sage", "Tate", "Vance",
+            "Zane", "Ash", "Cade", "Flynn", "Gray", "Jett", "Knox", "Leif", "Rhett", "Troy",
+            "Zion", "Axel", "Cyrus", "Dante", "Ezra", "Jasper", "Kian", "Luca", "Maddox", "Orion", "Ryder", "Silas", "Talon", "Zayden",
+            "Alaric", "Balthazar", "Cassian", "Dorian", "Evander", "Lucian", "Magnus", "Octavian", "Thaddeus", "Valerian",
+            "Zephyr", "Aldric", "Brennan", "Caius", "Dashiell", "Ezekiel", "Leander", "Matthias", "Sullivan", "Theron",
+            "Xavian", "Zander", "Archer", "Brennan", "Caspian"
+        };
+
+        List<string> neutralFirstFantasyNames = new List<string>()
+        {
+            "Bael", "Celes", "Dara", "Eira", "Fenn", "Gale", "Hale", "Ira", "Joss", "Kari", "Lior", "Mira", "Nia", "Orin", "Pax", "Quin", "Rae", "Sage",
+            "Wren", "Zephyr", "Ashen", "Briar", "Cyan", "Ember", "Frost", "Glade", "Haven", "Indigo", "Jade", "Lark", "Nova", "Onyx", "Rune",
+            "Skylar", "Vale", "Winter", "Zion", "Aeris", "Bryn", "Ciel", "Dusk", "Lioren", "Blaise", "Phoenix"
+        };
+
+        List<string> femaleFirstFantasyNames = new List<string>()
+        {
+            "Aria", "Brina", "Celia", "Dara", "Elara", "Fiona", "Gwen", "Helena", "Isla", "Jora", "Kira", "Luna", "Mira", "Nia", "Orla", "Phaedra", "Quilla", "Rhea", "Selene", "Thalia",
+            "Una", "Vera", "Wynne", "Xara", "Yara", "Zara",
+            "Ayla", "Bryn", "Cleo", "Daphne", "Eira", "Freya", "Gaia", "Hazel", "Ivy", "Juno", "Kaia", "Lila", "Maya", "Nora", "Opal", "Piper", "Quinn", "Raven",
+            "Sage", "Tessa", "Violet",
+            "Zoe", "Aurora", "Celeste", "Elowen", "Isolde", "Lyra", "Seraphina", "Thalassa",
+            "Amara", "Bellatrix", "Calista", "Dahlia", "Evangeline", "Lysandra", "Melisande",
+            "Valeria", 
+            "Zinnia", "Ariel", "Tali", "Vesper", "Elysia", "Faye"
+        };
         public NameGenerator()
         {
             InitializeComponent();
@@ -33,6 +63,19 @@ namespace RPGUtility.Pages
         private void addMemoButton_Click(object sender, RoutedEventArgs e)
         {
 
+        }
+
+        private void typeComboBox_SelectionChanged(object sender, SelectionChangedEventArgs e)
+        {
+            if (genderComboBox == null) return;
+            if (typeComboBox.SelectedItem == fantasyPlaceCbi 
+                || typeComboBox.SelectedItem == modernPlaceCbi 
+                || typeComboBox.SelectedItem == scifiPlaceCbi
+                || typeComboBox.SelectedItem == monsterCbi)
+            {
+                genderComboBox.IsEnabled = false;
+            }
+            else genderComboBox.IsEnabled = true;
         }
     }
 }

a0a7d8c8b400038cc7339fb5920055465d75dbf0
digydigdogdead <carlyle.laurent@gmail.com>
2025-08-30 13:47:07 +0100

    Implement Modern Person name generation

--- WpfApp1/Pages/NameGenerator.xaml.cs
+++ WpfApp1/Pages/NameGenerator.xaml.cs
@@ -71,6 +71,59 @@ namespace RPGUtility.Pages
             "the Visionary", "the Prophet", "the Oracle", "the Seer", "the Mystic", "the Hermit", "the Recluse", "the Wanderer",
             "the Pilgrim", "the Sojourner", "the Traveler", "the Wayfarer", "the Adventurer", "the Hero", "the Legend", "the Myth", "the Fable"
         };
+        List<string> maleFirstModernNames = new List<string>()
+        {
+            "James", "John", "Robert", "Michael", "William", "David", "Richard", "Joseph", "Charles", "Thomas",
+            "Christopher", "Daniel", "Matthew", "Anthony", "Mark", "Donald", "Steven", "Paul", "Andrew", "Joshua",
+            "Kenneth", "Kevin", "Brian", "George", "Edward", "Ronald", "Timothy", "Jason", "Jeffrey", "Ryan",
+            "Jacob", "Gary", "Nicholas", "Eric", "Stephen", "Jonathan", "Larry", "Justin", "Scott", "Brandon",
+            "Benjamin", "Samuel", "Gregory", "Frank", "Alexander", "Raymond", "Patrick", "Jack",
+            "Dennis", "Jerry", "Tyler", "Aaron", "Jose", "Henry", "Adam", "Douglas", "Nathan", "Peter",
+            "Zachary", "Kyle", "Walter", "Harold", "Jeremy", "Ethan", "Carl", "Keith", "Roger", "Gerald",
+            "Christian", "Terry", "Sean", "Arthur", "Austin", "Noah", "Jesse", "Joe", "Bryan", "Billy",
+            "Jordan", "Albert", "Dylan", "Bruce", "Willie", "Gabriel", "Alan", "Juan", "Logan", "Wayne",
+            "Ralph", "Roy", "Eugene", "Randy", "Vincent", "Russell", "Louis", "Philip", "Bobby", "Johnny", "Bradley",
+            "Tim"
+        };
+        List<string> femaleFirstModernNames = new List<string>()
+        {
+            "Mary", "Patricia", "Jennifer", "Linda", "Elizabeth", "Barbara", "Susan", "Jessica", "Sarah", "Karen",
+            "Nancy", "Margaret", "Lisa", "Betty", "Dorothy", "Sandra", "Ashley", "Kimberly", "Donna", "Emily",
+            "Michelle", "Carol", "Amanda", "Melissa", "Deborah", "Stephanie", "Rebecca", "Sharon", "Laura", "Cynthia",
+            "Kathleen", "Amy", "Shirley", "Angela", "Helen", "Anna", "Brenda", "Pamela", "Nicole", "Emma",
+            "Samantha", "Katherine", "Christine", "Debra", "Rachel", "Catherine", "Carolyn", "Janet",
+            "Ruth", "Maria", "Heather", "Diane", "Virginia", "Julie", "Joyce", "Victoria", "Olivia",
+            "Kelly", "Christina", "Lauren", "Joan", "Evelyn", "Judith", "Megan", "Cheryl",
+            "Andrea", "Hannah","Jacqueline","Martha","Gloria","Teresa","Ann","Sara","Madison","Frances",
+            "Kathryn","Janice","Jean","Abigail","Alice","Julia","Judy","Sophia","Grace","Denise","Amber",
+            "Doris","Marilyn","Danielle","Beverly","Isabella","Theresa","Diana","Natalie","Brittany",
+            "Charlotte","Marie","Kayla","Alexis","Lori"
+        };
+        List<string> neutralFirstModernNames = new List<string>()
+        {
+            "Taylor", "Jordan", "Morgan", "Casey", "Riley", "Cameron", "Avery", "Quinn", "Reese", "Peyton",
+            "Alex", "Charlie", "Dakota", "Emerson", "Finley", "Hayden", "Jaden", "Kai", "Logan", "Micah",
+            "Parker", "Rowan", "Sawyer", "Skyler", "Tatum", "Blake", "Elliot", "Harper", "Justice", "Kendall",
+            "Lane", "Marley", "River", "Sage", "Shiloh", "Terry", "Valentine",
+            "Adrian", "Ariel", "Cruz", "Dallas", "Emery", "Frankie", "Indigo", "Jules",
+            "Lennon", "Marlowe", "Nico", "Oakley", "Remy", "Sloane"
+        };
+        List<string> modernSurnames = new List<string>()
+        {
+            "Smith", "Johnson", "Williams", "Brown", "Jones", "Garcia", "Miller", "Davis", "Rodriguez", "Martinez",
+            "Hernandez", "Lopez", "Gonzalez", "Wilson", "Anderson", "Thomas", "Taylor", "Moore", "Jackson", "Martin",
+            "Lee", "Perez", "Thompson", "White", "Harris", "Sanchez", "Clark", "Ramirez", "Lewis", "Robinson",
+            "Walker", "Young", "Allen", "King", "Wright", "Scott", "Torres", "Nguyen", "Hill", "Flores",
+            "Green", "Adams", "Nelson", "Baker", "Hall", "Rivera", "Campbell", "Mitchell", "Carter",
+            "Roberts", "Gomez", "Phillips", "Evans", "Turner", "Diaz", "Parker", "Cruz", "Edwards",
+            "Collins",  "Reyes","Stewart","Morris","Morales","Murphy","Cook","Rogers","Gutierrez","Ortiz",
+            "Morgan","Cooper","Peterson","Bailey","Reed","Kelly","Howard","Ramos","Kim","Cox","Ward",
+            "Richardson","Watson","Brooks","Chavez","Wood","James","Bennett","Gray","Mendoza","Ruiz",
+            "Hughes","Price","Alvarez","Castillo","Sanders","Patel","Myers", "Long","Ross","Foster","Jimenez",
+            "Powell","Jenkins","Perry","Russell","Sullivan","Bell","Coleman","Butler","Henderson","Barnes","Gonzales", "Laurent",
+            "Herforth", "Kreiger", "Lemieux", "Marchand", "Moreau", "Ouellet", "Pelletier", "Renaud", "Tremblay",
+            "Aldridge"
+        };
         public NameGenerator()
         {
             InitializeComponent();
@@ -102,11 +155,33 @@ namespace RPGUtility.Pages
                     var allNames = maleFirstFantasyNames.Concat(femaleFirstFantasyNames).Concat(neutralFirstFantasyNames).ToList();
                     firstName = allNames[rand.Next(allNames.Count)];
                 }
-                    nameTextBox.Text = $"{firstName} {epithet}";
+                nameTextBox.Text = $"{firstName} {epithet}";
             }
-            else
+            else if (typeComboBox.SelectedItem == modernPersonCbi)
             {
-                return;
+                Random rand = new Random();
+                string firstName = "";
+                string lastName = modernSurnames[rand.Next(modernSurnames.Count)];
+                if (genderComboBox.SelectedItem == maleCbi)
+                {
+                    var joinedNames = maleFirstModernNames.Concat(neutralFirstModernNames).ToList();
+                    firstName = joinedNames[rand.Next(joinedNames.Count)];
+                }
+                else if (genderComboBox.SelectedItem == femaleCbi)
+                {
+                    var joinedNames = femaleFirstModernNames.Concat(neutralFirstModernNames).ToList();
+                    firstName = joinedNames[rand.Next(joinedNames.Count)];
+                }
+                else if (genderComboBox.SelectedItem == neutralCbi)
+                {
+                    firstName = neutralFirstModernNames[rand.Next(neutralFirstModernNames.Count)];
+                }
+                else if (genderComboBox.SelectedItem == anyCbi)
+                {
+                    var allNames = maleFirstModernNames.Concat(femaleFirstModernNames).Concat(neutralFirstModernNames).ToList();
+                    firstName = allNames[rand.Next(allNames.Count)];
+                }
+                nameTextBox.Text = $"{firstName} {lastName}";
             }
         }
 

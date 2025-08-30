1aa6a85a52778f49a5a7bee541614f297af3a820
digydigdogdead <carlyle.laurent@gmail.com>
2025-08-30 13:33:05 +0100

    Implement fantasy person name generation

--- WpfApp1/Pages/NameGenerator.xaml.cs
+++ WpfApp1/Pages/NameGenerator.xaml.cs
@@ -50,6 +50,27 @@ namespace RPGUtility.Pages
             "Valeria", 
             "Zinnia", "Ariel", "Tali", "Vesper", "Elysia", "Faye"
         };
+
+        List<string> fantasyEpithets = new List<string>()
+        {
+            "the Brave", "the Wise", "the Swift", "the Bold", "the Just", "the Fierce", "the Cunning", "the Valiant", "the Noble", "the Strong",
+            "the Fearless", "the Merciful", "the Radiant", "the Resolute", "the Stalwart", "the Gallant", "the Mighty", "the Loyal",
+            "the Protector", "the Wanderer", "the Seeker", "the Enchanter", "the Shadow", "the Flame", "the Storm", "the Guardian",
+            "the Wanderer", "the Mystic", "the Alchemist", "the Sage", "the Herald", "the Champion", "the Conqueror", "the Liberator", "the Vanquisher",
+            "the Justicar", "the Paladin", "the Warden", "the Sentinel", "the Crusader", "the Invincible", "the Unyielding",
+            "the Indomitable", "the Resolute", "the Vengeful", "the Relentless", "the Fierceheart", "the Ironclad", "the Stormbringer",
+            "the Lightbringer", "the Shadowbane", "the Dawnbringer", "the Nightstalker", "the Earthshaker", "the Skybreaker",
+            "the Tidecaller", "the Unruly", "the Untamed", "the Wild", "the Savage", "the Ferocious", "the Beastmaster", "the Dragonborn",
+            "the Phoenix", "the Griffin", "the Wyvern", "the Basilisk", "the Chimera", "the Hydra", "the Leviathan", "the Kraken",
+            "the Serpent", "the Wyrm", "the Lich", "the Necromancer", "the Warlock", "the Sorcerer", "the Enchanter", "the Illusionist",
+            "the Summoner", "the Elementalist", "the Druid", "the Shaman", "the Ranger", "the Hunter", "the Archer", "the Scout",
+            "the Assassin", "the Thief", "the Rogue", "the Bard", "the Minstrel", "the Jester", "the Trickster", "the Alchemist",
+            "the Inventor", "the Engineer", "the Tinkerer", "the Artificer", "the Smith", "the Blacksmith", "the Armorer", "the Fletcher",
+            "the Cook", "the Brewer", "the Merchant", "the Trader", "the Sailor", "the Navigator", "the Captain", "the Admiral",
+            "the Explorer", "the Pioneer", "the Pathfinder", "the Voyager", "the Nomad", "the Pilgrim", "the Seeker", "the Dreamer",
+            "the Visionary", "the Prophet", "the Oracle", "the Seer", "the Mystic", "the Hermit", "the Recluse", "the Wanderer",
+            "the Pilgrim", "the Sojourner", "the Traveler", "the Wayfarer", "the Adventurer", "the Hero", "the Legend", "the Myth", "the Fable"
+        };
         public NameGenerator()
         {
             InitializeComponent();
@@ -57,7 +78,36 @@ namespace RPGUtility.Pages
 
         private void generateNameButton_Click(object sender, RoutedEventArgs e)
         {
-
+            if (typeComboBox.SelectedItem == fantasyPersonCbi)
+            {
+                Random rand = new Random();
+                string firstName = "";
+                string epithet = fantasyEpithets[rand.Next(fantasyEpithets.Count)];
+                if (genderComboBox.SelectedItem == maleCbi)
+                {
+                    var joinedNames = maleFirstFantasyNames.Concat(neutralFirstFantasyNames).ToList();
+                    firstName = joinedNames[rand.Next(joinedNames.Count)];
+                }
+                else if (genderComboBox.SelectedItem == femaleCbi)
+                {
+                    var joinedNames = femaleFirstFantasyNames.Concat(neutralFirstFantasyNames).ToList();
+                    firstName = joinedNames[rand.Next(joinedNames.Count)];
+                }
+                else if (genderComboBox.SelectedItem == neutralCbi)
+                {
+                    firstName = neutralFirstFantasyNames[rand.Next(neutralFirstFantasyNames.Count)];
+                }
+                else
+                {
+                    var allNames = maleFirstFantasyNames.Concat(femaleFirstFantasyNames).Concat(neutralFirstFantasyNames).ToList();
+                    firstName = allNames[rand.Next(allNames.Count)];
+                }
+                    nameTextBox.Text = $"{firstName} {epithet}";
+            }
+            else
+            {
+                return;
+            }
         }
 
         private void addMemoButton_Click(object sender, RoutedEventArgs e)

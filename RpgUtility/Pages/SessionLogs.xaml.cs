694ffe7bc75f129fa2dcefb4d31d3f372dc09ba6
digydigdogdead <carlyle.laurent@gmail.com>
2025-09-01 14:41:00 +0100

    Cleanup SessionLogs.xaml.cs

--- RpgUtility/Pages/SessionLogs.xaml.cs
+++ RpgUtility/Pages/SessionLogs.xaml.cs
@@ -1,17 +1,6 @@
-﻿using System;
-using System.Collections.Generic;
-using System.Linq;
-using System.Text;
-using System.Threading.Tasks;
+﻿using RPGUtility.Windows;
 using System.Windows;
 using System.Windows.Controls;
-using System.Windows.Data;
-using System.Windows.Documents;
-using System.Windows.Input;
-using System.Windows.Media;
-using System.Windows.Media.Imaging;
-using System.Windows.Navigation;
-using System.Windows.Shapes;
 
 namespace RPGUtility.Pages
 {
@@ -29,8 +18,7 @@ namespace RPGUtility.Pages
 
         private void addSessionLogButton_Click(object sender, RoutedEventArgs e)
         {
-            Windows.NewLogWindow newLogWindow = new Windows.NewLogWindow();
-            newLogWindow.ShowDialog();
+            new NewLogWindow().Show();
         }
 
         public void RefreshLogs()

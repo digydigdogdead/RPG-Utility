2bd071a863b20bf202c7f0c12ea46924119daa19
digydigdogdead <carlyle.laurent@gmail.com>
2025-09-04 17:03:15 +0100

    Complete scaffolding of Household roller preset

---
+++ RpgUtility/Pages/DiceRollerPresets/HouseholdDiceRoller.xaml.cs
@@ -0,0 +1,33 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows;
+using System.Windows.Controls;
+using System.Windows.Data;
+using System.Windows.Documents;
+using System.Windows.Input;
+using System.Windows.Media;
+using System.Windows.Media.Imaging;
+using System.Windows.Navigation;
+using System.Windows.Shapes;
+
+namespace RPGUtility.Pages.DiceRollerPresets
+{
+    /// <summary>
+    /// Interaction logic for HouseholdDiceRoller.xaml
+    /// </summary>
+    public partial class HouseholdDiceRoller : Page
+    {
+        public HouseholdDiceRoller()
+        {
+            InitializeComponent();
+        }
+
+        private void rollButton_Click(object sender, RoutedEventArgs e)
+        {
+
+        }
+    }
+}

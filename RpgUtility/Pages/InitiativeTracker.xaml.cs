c1e7f9944675df077ec2165717daa2d36b09c90f
digydigdogdead <carlyle.laurent@gmail.com>
2025-09-04 12:00:59 +0100

    Complete scaffolding for initiative tracker page and tab

---
+++ RpgUtility/Pages/InitiativeTracker.xaml.cs
@@ -0,0 +1,48 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows;
+using System.Windows.Controls;
+using System.Windows.Data;
+using System.Windows.Documents;
+using System.Windows.Input;
+using System.Windows.Media;
+using System.Windows.Media.Imaging;
+using System.Windows.Navigation;
+using System.Windows.Shapes;
+
+namespace RPGUtility.Pages
+{
+    /// <summary>
+    /// Interaction logic for InitiativeTracker.xaml
+    /// </summary>
+    public partial class InitiativeTracker : Page
+    {
+        public InitiativeTracker()
+        {
+            InitializeComponent();
+        }
+
+        private void addCombatantButton_Click(object sender, RoutedEventArgs e)
+        {
+
+        }
+
+        private void removeCombatantButton_Click(object sender, RoutedEventArgs e)
+        {
+
+        }
+
+        private void previousTurnButton_Click(object sender, RoutedEventArgs e)
+        {
+
+        }
+
+        private void nextTurnButton_Click(object sender, RoutedEventArgs e)
+        {
+
+        }
+    }
+}

60065cee063877bb22ed409ccd341412842a9edf
digydigdogdead <carlyle.laurent@gmail.com>
2025-09-28 19:18:34 +0200

    Complete scaffolding for Condition Manager Window

---
+++ RpgUtility/Windows/ConditionManager.xaml.cs
@@ -0,0 +1,37 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows;
+using System.Windows.Controls;
+using System.Windows.Data;
+using System.Windows.Documents;
+using System.Windows.Input;
+using System.Windows.Media;
+using System.Windows.Media.Imaging;
+using System.Windows.Shapes;
+
+namespace RPGUtility.Windows
+{
+    /// <summary>
+    /// Interaction logic for ConditionManager.xaml
+    /// </summary>
+    public partial class ConditionManager : Window
+    {
+        public ConditionManager()
+        {
+            InitializeComponent();
+        }
+
+        private void addConditionButton_Click(object sender, RoutedEventArgs e)
+        {
+
+        }
+
+        private void removeConditionButton_Click(object sender, RoutedEventArgs e)
+        {
+
+        }
+    }
+}

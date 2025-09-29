cd21bc4f899ef385efced457c4f71eb3a1f1a00f
digydigdogdead <carlyle.laurent@gmail.com>
2025-09-29 18:41:23 +0200

    Update SaveData class to work with new Combatant control

--- RpgUtility/Classes/SaveData.cs
+++ RpgUtility/Classes/SaveData.cs
@@ -10,12 +10,12 @@ namespace RPGUtility
 {
     public class SaveData
     {
-        public List<(int Segments, string Name, int Filled)> ClocksData { get; set; } = new List<(int Segments, string Name, int Filled)>();
-        public List<(string Name, int Value, Color BackgroundColour)> StatsData { get; set; } = new List<(string Name, int Value, Color BackgroundColor)>();
-        public List<(string Title, string Content)> MemosData { get; set; } = new List<(string Title, string Content)>();
-        public List<(int SessionNumber, string LogTitle, string SessionDescription)> SessionLogsData { get; set; } = new List<(int SessionNumber, string LogTitle, string SessionDescription)>();
+        public List<(int Segments, string Name, int Filled)> ClocksData { get; set; } = new();
+        public List<(string Name, int Value, Color BackgroundColour)> StatsData { get; set; } = new();
+        public List<(string Title, string Content)> MemosData { get; set; } = new();
+        public List<(int SessionNumber, string LogTitle, string SessionDescription)> SessionLogsData { get; set; } = new();
         public Dictionary<string, bool> TabsData { get; set; } = new Dictionary<string, bool>();
-        public List<Combatant> CombatantsData { get; set; } = new List<Combatant>();
+        public List<(string Name, int Initiative, int Hp, List<string> Conditions)> CombatantsData { get; set; } = new();
         public int CurrentTurnIndex { get; set; } = 0;
     }
 }

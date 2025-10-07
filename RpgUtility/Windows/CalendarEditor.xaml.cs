a56ae5a12887f12e45a9b2dc14c04ab5223552b9
digydigdogdead <carlyle.laurent@gmail.com>
2025-10-07 12:19:57 +0200

    Add logic for Use Gregorian Calendar button and Clear button to Calendar Editor

--- RpgUtility/Windows/CalendarEditor.xaml.cs
+++ RpgUtility/Windows/CalendarEditor.xaml.cs
@@ -30,6 +30,7 @@ namespace RPGUtility.Windows
             int daysInMonth = (int)DaysInMonthIntegerUpDown.Value!;
             var month = new { MonthName = monthName, DaysInMonth = daysInMonth };
             MonthsListView.Items.Add(month);
+            MonthNameTextBox.Clear();
         }
 
         private void RemoveMonthButton_Click(object sender, RoutedEventArgs e)
@@ -39,7 +40,29 @@ namespace RPGUtility.Windows
 
         private void UseGregorianButton_Click(object sender, RoutedEventArgs e)
         {
-
+            var january = new { MonthName = "January", DaysInMonth = 31 };
+            MonthsListView.Items.Add(january);
+            var february = new { MonthName = "February", DaysInMonth = 28 };
+            MonthsListView.Items.Add(february);
+            var march = new { MonthName = "March", DaysInMonth = 31 };
+            MonthsListView.Items.Add(march);
+            var april = new { MonthName = "April", DaysInMonth = 30 };
+            MonthsListView.Items.Add(april);
+            var may = new { MonthName = "May", DaysInMonth = 31 };
+            MonthsListView.Items.Add(may);
+            var june = new { MonthName = "June", DaysInMonth = 30 };
+            MonthsListView.Items.Add(june);
+            var july = new { MonthName = "July", DaysInMonth = 31 };
+            MonthsListView.Items.Add(july);
+            var august = new { MonthName = "August", DaysInMonth = 31 };
+            MonthsListView.Items.Add(august);
+            var september = new { MonthName = "September", DaysInMonth = 30 };
+            MonthsListView.Items.Add(september);
+            var october = new { MonthName = "October", DaysInMonth = 31 };
+            MonthsListView.Items.Add(october);
+            var november = new { MonthName = "November", DaysInMonth = 30 };
+            MonthsListView.Items.Add(november);
+            var december = new { MonthName = "December", DaysInMonth = 31 };
         }
 
         private void SaveCalendarButton_Click(object sender, RoutedEventArgs e)
@@ -53,6 +76,11 @@ namespace RPGUtility.Windows
             (App.Current as App)!.CurrentMonthIndex = 0;
             (App.Current as App)!.CalendarPage!.currentCalendar!.PopulateCalendar();
             this.Close();
+        }
+
+        private void ClearButton_Click(object sender, RoutedEventArgs e)
+        {
+            MonthsListView.Items.Clear();
         }
     }
 }

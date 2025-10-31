48346a53c3261ffc17b86d8672df048c5954e223
digydigdogdead <carlyle.laurent@gmail.com>
2025-10-31 10:15:36 +0000

    Reduce functions related to loading names from text files to what is necessary

--- RpgUtility/Pages/NameGenerator.xaml.cs
+++ RpgUtility/Pages/NameGenerator.xaml.cs
@@ -13,18 +13,18 @@ namespace RPGUtility.Pages
     /// </summary>
     public partial class NameGenerator : Page
     {
-        List<string> maleFirstFantasyNames = new List<string>() { };
-        List<string> neutralFirstFantasyNames = new List<string>() { };
-        List<string> femaleFirstFantasyNames = new List<string>() { };
-        List<string> fantasyEpithets = new List<string>() { };
-        List<string> maleFirstModernNames = new List<string>() { };
-        List<string> femaleFirstModernNames = new List<string>() { };
-        List<string> neutralFirstModernNames = new List<string>() { };
-        List<string> modernSurnames = new List<string>() { };
-        List<string> fantasyPlaceNames = new List<string>() { };
-        List<string> scifiPlaceNames = new List<string>() { };
-        List<string> modernPlaceNames = new List<string>() { };
-        List<string> monsterNames = new List<string>() { };
+        List<string>? maleFirstFantasyNames = new List<string>() { };
+        List<string>? neutralFirstFantasyNames = new List<string>() { };
+        List<string>? femaleFirstFantasyNames = new List<string>() { };
+        List<string>? fantasyEpithets = new List<string>() { };
+        List<string>? maleFirstModernNames = new List<string>() { };
+        List<string>? femaleFirstModernNames = new List<string>() { };
+        List<string>? neutralFirstModernNames = new List<string>() { };
+        List<string>? modernSurnames = new List<string>() { };
+        List<string>? fantasyPlaceNames = new List<string>() { };
+        List<string>? scifiPlaceNames = new List<string>() { };
+        List<string>? modernPlaceNames = new List<string>() { };
+        List<string>? monsterNames = new List<string>() { };      
 
         public NameGenerator()
         {
@@ -38,24 +38,24 @@ namespace RPGUtility.Pages
             {
                 Random rand = new Random();
                 string firstName = "";
-                string epithet = fantasyEpithets[rand.Next(fantasyEpithets.Count)];
+                string epithet = fantasyEpithets![rand.Next(fantasyEpithets.Count)];
                 if (genderComboBox.SelectedItem == maleCbi)
                 {
-                    var joinedNames = maleFirstFantasyNames.Concat(neutralFirstFantasyNames).ToList();
+                    var joinedNames = maleFirstFantasyNames!.Concat(neutralFirstFantasyNames!).ToList();
                     firstName = joinedNames[rand.Next(joinedNames.Count)];
                 }
                 else if (genderComboBox.SelectedItem == femaleCbi)
                 {
-                    var joinedNames = femaleFirstFantasyNames.Concat(neutralFirstFantasyNames).ToList();
+                    var joinedNames = femaleFirstFantasyNames!.Concat(neutralFirstFantasyNames!).ToList();
                     firstName = joinedNames[rand.Next(joinedNames.Count)];
                 }
                 else if (genderComboBox.SelectedItem == neutralCbi)
                 {
-                    firstName = neutralFirstFantasyNames[rand.Next(neutralFirstFantasyNames.Count)];
+                    firstName = neutralFirstFantasyNames![rand.Next(neutralFirstFantasyNames.Count)];
                 }
                 else
                 {
-                    var allNames = maleFirstFantasyNames.Concat(femaleFirstFantasyNames).Concat(neutralFirstFantasyNames).ToList();
+                    var allNames = maleFirstFantasyNames!.Concat(femaleFirstFantasyNames!).Concat(neutralFirstFantasyNames!).ToList();
                     firstName = allNames[rand.Next(allNames.Count)];
                 }
                 nameTextBox.Text = $"{firstName} the {epithet}";
@@ -64,24 +64,24 @@ namespace RPGUtility.Pages
             {
                 Random rand = new Random();
                 string firstName = "";
-                string lastName = modernSurnames[rand.Next(modernSurnames.Count)];
+                string lastName = modernSurnames![rand.Next(modernSurnames.Count)];
                 if (genderComboBox.SelectedItem == maleCbi)
                 {
-                    var joinedNames = maleFirstModernNames.Concat(neutralFirstModernNames).ToList();
+                    var joinedNames = maleFirstModernNames!.Concat(neutralFirstModernNames!).ToList();
                     firstName = joinedNames[rand.Next(joinedNames.Count)];
                 }
                 else if (genderComboBox.SelectedItem == femaleCbi)
                 {
-                    var joinedNames = femaleFirstModernNames.Concat(neutralFirstModernNames).ToList();
+                    var joinedNames = femaleFirstModernNames!.Concat(neutralFirstModernNames!).ToList();
                     firstName = joinedNames[rand.Next(joinedNames.Count)];
                 }
                 else if (genderComboBox.SelectedItem == neutralCbi)
                 {
-                    firstName = neutralFirstModernNames[rand.Next(neutralFirstModernNames.Count)];
+                    firstName = neutralFirstModernNames![rand.Next(neutralFirstModernNames.Count)];
                 }
                 else if (genderComboBox.SelectedItem == anyCbi)
                 {
-                    var allNames = maleFirstModernNames.Concat(femaleFirstModernNames).Concat(neutralFirstModernNames).ToList();
+                    var allNames = maleFirstModernNames!.Concat(femaleFirstModernNames!).Concat(neutralFirstModernNames!).ToList();
                     firstName = allNames[rand.Next(allNames.Count)];
                 }
                 nameTextBox.Text = $"{firstName} {lastName}";
@@ -148,78 +148,91 @@ namespace RPGUtility.Pages
 
         public void LoadNames()
         {
-            // make paths robust at runtime (use AppContext.BaseDirectory)
-            string dataDir = Path.Combine(AppContext.BaseDirectory, "Random Names");
+            List<string> errors = new List<string>();
 
-            var missing = new List<string>();
+            try
+            {
+                fantasyEpithets = ReadNameFile("fantasy_epithets.txt");
+            }
+            catch
+            { errors.Add("fantasy_epithets.txt"); }
+            try             
+            {
+                fantasyPlaceNames = ReadNameFile("fantasy_place_names.txt");
+            }
+            catch { errors.Add("fantasy_place_names.txt"); }
+            try
+            {
+                femaleFirstFantasyNames = ReadNameFile("female_fantasy_first_names.txt");
+            }
+            catch
+            { errors.Add("female_fantasy_first_names.txt"); }
+            try
+            {
+                femaleFirstModernNames = ReadNameFile("female_modern_first_names.txt");
+            }
+            catch { errors.Add("female_modern_first_names.txt"); }
+            try
+            {
+                maleFirstFantasyNames = ReadNameFile("male_fantasy_first_names.txt");
+            }
+            catch { errors.Add("male_first_fantasy_names.txt"); }
+            try
+            {
+                maleFirstModernNames = ReadNameFile("male_modern_first_names.txt");
+            }
+            catch
+            {
+                errors.Add("male_modern_first_names.txt");
+            }
+            try             
+            {
+                modernPlaceNames = ReadNameFile("modern_place_names.txt");
+            }
+            catch { errors.Add("modern_place_names.txt"); }
+            try             
+            {
+                modernSurnames = ReadNameFile("modern_surnames.txt");
+            }
+            catch { errors.Add("modern_surnames.txt"); }
+            try             
+            {
+                monsterNames = ReadNameFile("monster_names.txt");
+            }
+            catch { errors.Add("monster_names.txt"); }
+            try
+            {
+                neutralFirstFantasyNames = ReadNameFile("neutral_fantasy_first_names.txt");
+            }
+            catch
+            { errors.Add("neutral_fantasy_first_names.txt"); }
+            try             
+            {
+                neutralFirstModernNames = ReadNameFile("neutral_modern_first_names.txt");
+            }
+            catch { errors.Add("neutral_modern_first_names.txt"); }
+            try             
+            {
+                scifiPlaceNames = ReadNameFile("scifi_place_names.txt");
+            }
+            catch { errors.Add("scifi_place_names.txt"); }
 
-            maleFirstFantasyNames = ReadLinesSafe(Path.Combine(dataDir, "male_fantasy_first_names.txt"), missing);
-            femaleFirstFantasyNames = ReadLinesSafe(Path.Combine(dataDir, "female_fantasy_first_names.txt"), missing);
-            neutralFirstFantasyNames = ReadLinesSafe(Path.Combine(dataDir, "neutral_fantasy_first_names.txt"), missing);
-            fantasyEpithets = ReadLinesSafe(Path.Combine(dataDir, "fantasy_epithets.txt"), missing);
-
-            maleFirstModernNames = ReadLinesSafe(Path.Combine(dataDir, "male_modern_first_names.txt"), missing);
-            femaleFirstModernNames = ReadLinesSafe(Path.Combine(dataDir, "female_modern_first_names.txt"), missing);
-            neutralFirstModernNames = ReadLinesSafe(Path.Combine(dataDir, "neutral_modern_first_names.txt"), missing);
-            modernSurnames = ReadLinesSafe(Path.Combine(dataDir, "modern_surnames.txt"), missing);
-
-            fantasyPlaceNames = ReadLinesSafe(Path.Combine(dataDir, "fantasy_place_names.txt"), missing);
-            scifiPlaceNames = ReadLinesSafe(Path.Combine(dataDir, "scifi_place_names.txt"), missing);
-            modernPlaceNames = ReadLinesSafe(Path.Combine(dataDir, "modern_place_names.txt"), missing);
-            monsterNames = ReadLinesSafe(Path.Combine(dataDir, "monster_names.txt"), missing);
-
-            if (missing.Any())
+            if (errors.Count > 0)
             {
                 MessageBox.Show(
-                    $"Could not find {missing.Count} data file(s).\n\nCurrentDir: {Directory.GetCurrentDirectory()}\nBaseDir: {AppContext.BaseDirectory}\n\nMissing:\n{string.Join("\n", missing)}",
+                    $"Could not find {errors.Count} data file(s).\n\nCurrentDir: {Directory.GetCurrentDirectory()}\nBaseDir: {AppContext.BaseDirectory}\n\nMissing:\n{string.Join("\n", errors)}",
                     "Data files not found", MessageBoxButton.OK, MessageBoxImage.Warning);
             }
         }
 
-        private List<string> ReadLinesSafe(string relativePath, List<string> missing)
+        private List<string>? ReadNameFile(string name)
         {
-            try
-            {
-                var full = ResolveDataPath(relativePath);
-                return File.ReadAllLines(full).ToList();
-            }
-            catch (FileNotFoundException)
-            {
-                missing?.Add(relativePath);
-                return new List<string>();
-            }
-            catch (Exception ex)
-            {
-                MessageBox.Show($"Error reading '{relativePath}': {ex.Message}", "Read error", MessageBoxButton.OK, MessageBoxImage.Error);
-                missing?.Add(relativePath);
-                return new List<string>();
-            }
-        }
+            string directory = Path.Combine(AppContext.BaseDirectory, "Random Names");
 
-        private string ResolveDataPath(string relativePath)
-        {
-            // Try a set of sensible locations (exe base, working dir, assembly location, a couple up-level guesses)
-            var baseDir = AppContext.BaseDirectory;
-            var asmDir = Path.GetDirectoryName(Assembly.GetExecutingAssembly().Location) ?? baseDir;
-            var candidates = new[]
-            {
-                Path.Combine(baseDir, relativePath),
-                Path.Combine(Directory.GetCurrentDirectory(), relativePath),
-                Path.Combine(asmDir, relativePath),
-                Path.GetFullPath(Path.Combine(baseDir, "..", relativePath)),
-                Path.GetFullPath(Path.Combine(baseDir, "..", "..", relativePath))
-            };
+            string filePath = Path.Combine(directory, name);
 
-            foreach (var c in candidates)
-            {
-                try
-                {
-                    if (File.Exists(c)) return c;
-                }
-                catch { /* ignore malformed paths */ }
-            }
-
-            throw new FileNotFoundException($"Data file not found: '{relativePath}'. Searched {candidates.Length} locations.");
+            if (File.Exists(filePath)) { return File.ReadAllLines(filePath).ToList(); }
+            else { return null; }
         }
 
         private void helpButton_Click(object sender, RoutedEventArgs e)
